3c4b04d8c3b566d608c2d03a5820579388d0dc40
Alyssa <akmorrow@wisc.edu>
2015-03-11 21:46:43 -0500

    fb login

--- www/js/validate.js
+++ www/js/validate.js
@@ -15,3 +15,16 @@ function validateCredentials() {
 function validateLogin(){
     validateCredentials();
 }
+
+function loginFacebook() {
+ FB.login(function(response) {
+   if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ });
+}
